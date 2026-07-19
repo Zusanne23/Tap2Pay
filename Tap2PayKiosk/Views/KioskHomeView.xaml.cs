378f91c54b1a7b80f17090e19d228f33cc2ef404
Zusanne23 <143505639+Zusanne23@users.noreply.github.com>
2026-07-19 13:23:23 +0800

    Add files via upload

---
+++ Tap2PayKiosk/Views/KioskHomeView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tap2PayKiosk.Views
+{
+    public partial class KioskHomeView : Window
+    {
+        public KioskHomeView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOrderFood_Click(object sender, RoutedEventArgs e)
+        {
+            OrderView order = new OrderView();
+            order.Show();
+            Close();
+        }
+
+        private void btnCheckBalance_Click(object sender, RoutedEventArgs e)
+        {
+            CheckBalanceView balance = new CheckBalanceView();
+            balance.Show();
+            Close();
+        }
+    }
+}
